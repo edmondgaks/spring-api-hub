e01c25bd9fb7ee26494ee6479f27891beb3e5fdc
edmondgaks <edmondgaks@gmail.com>
2023-02-21 16:04:35 +0200

    dependency injection

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,24 +1,23 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
 @RestController
-@RequestMapping(path="/hello")
+@RequestMapping(path="/")
 public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@GetMapping
-	public List<String> hello() {
-		return List.of("Hello","world");
-	}
 
 }
