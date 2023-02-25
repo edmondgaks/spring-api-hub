651c9c9a11f0bdb07fda4abcda63eff9c70a1c93
edmondgaks <edmondgaks@gmail.com>
2023-02-25 11:34:07 +0200

    testing but first cleaning

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
-
+;
 @Repository
 public interface StudentRepository extends JpaRepository<Student,Long> {
     @Query("Select s FROM Student s WHERE s.email = ?1")
